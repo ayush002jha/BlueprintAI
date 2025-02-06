665f8b07bf58ec8f04eb124e481ff56654285c21
ayush002jha <ayush9494jha@outlook.com>
2025-02-06 22:58:10 +0530

    Integrates Vercel AI SDK

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,30 @@
-import Image from "next/image";
+'use client';
 
-export default function Home() {
+
+import { useChat } from 'ai/react';
+
+export default function Chat() {
+  const { messages, input, handleInputChange, handleSubmit } = useChat();
   return (
-    <div className="min-h-screen flex items-center justify-center">
+    <div className="flex flex-col w-full max-w-md py-24 mx-auto stretch">
       <h1 className="text-6xl font-bold tracking-tight font-[family-name:var(--font-space-grotesk)]">
         Blueprint AI
       </h1>
+      {messages.map(m => (
+        <div key={m.id} className="whitespace-pre-wrap">
+          {m.role === 'user' ? 'User: ' : 'AI: '}
+          {m.content}
+        </div>
+      ))}
+
+      <form onSubmit={handleSubmit}>
+        <input
+          className="fixed dark:bg-zinc-900 bottom-0 w-full max-w-md p-2 mb-8 border border-zinc-300 dark:border-zinc-800 rounded shadow-xl"
+          value={input}
+          placeholder="Say something..."
+          onChange={handleInputChange}
+        />
+      </form>
     </div>
   );
 }
-
