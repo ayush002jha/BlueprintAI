cbda2dfdf2929565179c9ab6fe50c094a045fa52
ayush002jha <ayush9494jha@outlook.com>
2025-02-12 02:24:12 +0530

    Button Colour UI FIX

--- src/components/Blueprint.tsx
+++ src/components/Blueprint.tsx
@@ -66,7 +66,7 @@ const PersonalizedBlueprint = () => {
             </button>
             <button
               onClick={handleRegenerate}
-              className="px-8 py-0.5 border-2 text-[#0077CC] border-black dark:border-white uppercase bg-white text-black transition duration-200 text-sm shadow-[1px_1px_rgba(0,0,0),2px_2px_rgba(0,0,0),3px_3px_rgba(0,0,0),4px_4px_rgba(0,0,0),5px_5px_0px_0px_rgba(0,0,0)] dark:shadow-[1px_1px_rgba(255,255,255),2px_2px_rgba(255,255,255),3px_3px_rgba(255,255,255),4px_4px_rgba(255,255,255),5px_5px_0px_0px_rgba(255,255,255)]"
+              className="px-8 py-0.5 border-2 text-[#0077CC] border-black dark:border-white uppercase bg-white  transition duration-200 text-sm shadow-[1px_1px_rgba(0,0,0),2px_2px_rgba(0,0,0),3px_3px_rgba(0,0,0),4px_4px_rgba(0,0,0),5px_5px_0px_0px_rgba(0,0,0)] dark:shadow-[1px_1px_rgba(255,255,255),2px_2px_rgba(255,255,255),3px_3px_rgba(255,255,255),4px_4px_rgba(255,255,255),5px_5px_0px_0px_rgba(255,255,255)]"
             >
               Download Blueprint
             </button>
